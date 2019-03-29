54eaf1f01768b897f6f87de0077452f248970e5a
Natalia <natalia.lyaskina@opencastsoftware.com>
2019-03-29 12:29:01 +0000

    Initial commit of my components without tests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-
+import React, { Component } from 'react'
+import Header from './component/header';
+import './app.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+      <Header />
       </div>
     );
   }
-}
+} 
 
 export default App;
