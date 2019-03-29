0034cb03f45aae0c9465379c716d6f36275f5b7b
Natalia <natalia.lyaskina@opencastsoftware.com>
2019-03-29 17:41:42 +0000

    Addit propTypes to our Headline component

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,10 @@
-
+import checkPropTypes from 'check-prop-types';
 
 export const findByTestAttr = (component,attr) => {
     const element = component.find(`[data-test='${attr}']`);
     return element;
+};
+
+export const checkProps = (component, expectedProps) => {
+    const propsErr = checkPropsTypes(component.propTypes, expectedProps, 'props', component.name);
 };
